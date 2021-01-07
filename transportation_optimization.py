c7b151f9e411820e69813bfa73f85c6d39a6d131
chenshaobin <13531194616@163.com>
2021-01-07 21:36:07 +0800

    update

--- transportation_optimization.py
+++ transportation_optimization.py
@@ -3,7 +3,7 @@ from cvxpy import *
 import cvxpy as cvx
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # 空间三维画图
-
+from matplotlib.animation import FuncAnimation
 m = 6
 n = 6
 totals = [60, 60]
@@ -46,19 +46,21 @@ wareHouse_index = ["house_" + str(i) for i in range(n)]
 # print(fac_anotatation)
 # plot factories_output and wareHouse_demand
 #"""
+# figure of Factories Products、WareHouse Demands、Location
+# """
 plt.figure(figsize=(30, 30))
-plt.subplot(2, 2, 1)
+plt.subplot(1, 2, 1)
 plt.scatter(fac_index, factories_output, marker=6)
 plt.title('Factories Products')
 plt.xlabel('Factories')
 plt.ylabel('Plant Output')
 
-plt.subplot(2, 2, 2)
+plt.subplot(1, 2, 2)
 plt.scatter(wareHouse_index, wareHouse_demand, marker=7)
 plt.title('WareHouse Demands')
 plt.xlabel('WareHouses')
 plt.ylabel('Demands')
-
+"""
 plt.subplot(2, 2, 3)
 plt.scatter(factories_location[:,0], factories_location[:,1], marker='.', color='red', alpha=0.5, label='factory')
 plt.scatter(wareHouse_location[:,0], wareHouse_location[:,1], marker='*', color='blue', alpha=0.5, label='wareHouse')
@@ -66,9 +68,11 @@ plt.title('Location ')
 plt.xlabel('x')
 plt.ylabel( 'y')
 plt.legend(loc='upper right')
+"""
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                 wspace=0.2, hspace=0.5)
 plt.show()
+# """
 # for i in range(len(fac_anotatation)):
 #     plt.annotate(fac_anotatation[i], xy=(fac_index[i], factories_output[i]), xytext=(fac_index[i]+0.1, factories_output[i]+0.1)) # 这里xy是需要标记的坐标，xytext是对应的标签坐标
 
@@ -114,6 +118,7 @@ print(result)
 # print(np.rint(x.value))
 fac_index = [i for i in range(m)]
 wareHouse_index = [i for i in range(n)]
+"""
 color = ['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 fig = plt.figure()
 ax = Axes3D(fig)
@@ -124,4 +129,65 @@ ax.set_zlabel('Product', fontdict={'size': 15, 'color': 'red'})
 ax.set_ylabel('House', fontdict={'size': 15, 'color': 'red'})
 ax.set_xlabel('Fac', fontdict={'size': 15, 'color': 'red'})
 
+plt.show()
+"""
+
+
+
+
+fig = plt.figure(figsize=(15, 15))
+plt.scatter(factories_location[:,0], factories_location[:,1], marker='.', color='red', alpha=0.5, label='factory')
+plt.scatter(wareHouse_location[:,0], wareHouse_location[:,1], marker='*', color='blue', alpha=0.5, label='wareHouse')
+plt.title('Location ')
+plt.xlabel('x')
+plt.ylabel( 'y')
+plt.legend(loc='upper right')
+color = ['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+lineShape = ['solid', 'dashed', 'dashdot', 'dotted','--', '-.']
+# """
+for i in range(m):
+    x1 = factories_location[i, 0]
+    y1 = factories_location[i, 1]
+    for j in range(n):
+        shiftNumber = result[i, j]
+        if shiftNumber != 0:
+            x2 = wareHouse_location[j, 0]
+            y2 = wareHouse_location[j, 1]
+            # ls :'-', '--', '-.', ':', 'None', ' ', '', 'solid', 'dashed', 'dashdot', 'dotted'
+            plt.plot([x1, x2], [y1, y2], c=color[j],ls=lineShape[i])
+            x_annotate = (x1 + x2) / 2
+            y_annotate = (y1 + y2) / 2
+            plt.annotate(text='%s' % shiftNumber, xy=(x_annotate, y_annotate),
+                         xytext=(x_annotate, y_annotate), textcoords='offset pixels', xycoords='data',color=color[j])
+# """
+
+"""
+ln, = plt.plot([], [])
+def update(frame):
+    for i in range(m):
+        x1 = factories_location[i, 0]
+        y1 = factories_location[i, 1]
+        for j in range(n):
+            shiftNumber = result[i, j]
+            if shiftNumber != 0:
+                x2 = wareHouse_location[j, 0]
+                y2 = wareHouse_location[j, 1]
+                xData = [x1, x2]
+                yData = [y1, y2]
+                ln.set_data = (xData, yData)
+                ln.c = color[j]
+                ln.ls = lineShape[i]
+                # plt.plot([x1, x2], [y1, y2], c=color[j], ls=lineShape[i])
+                x_annotate = (x1 + x2) / 2
+                y_annotate = (y1 + y2) / 2
+                plt.annotate(text='%s' % shiftNumber, xy=(x_annotate, y_annotate),
+                             xytext=(x_annotate, y_annotate), textcoords='offset pixels', xycoords='data',
+                             color=color[j])
+                return ln,
+ani = FuncAnimation(fig, update,blit=True, interval=1)
+"""
+# plt.plot([factories_location[0,0],wareHouse_location[0,0]],[factories_location[0,1],wareHouse_location[0,1]],c='blue',ls='--')
+# plt.annotate(text='10',xy=((factories_location[0,0] + wareHouse_location[0,0])/2, (factories_location[0,1] + wareHouse_location[0,1]) / 2),
+#              xytext= ((factories_location[0,0] + wareHouse_location[0,0])/2, (factories_location[0,1] + wareHouse_location[0,1]) / 2),
+#              textcoords='offset points')
 plt.show()
