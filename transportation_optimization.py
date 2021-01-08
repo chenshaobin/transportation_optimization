ef7be206fdfbf3be86af99e84c841b42792225af
chenshaobin <13531194616@163.com>
2021-01-08 11:39:46 +0800

    update

--- transportation_optimization.py
+++ transportation_optimization.py
@@ -2,7 +2,6 @@ import numpy as np
 from cvxpy import *
 import cvxpy as cvx
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  # 空间三维画图
 from matplotlib.animation import FuncAnimation
 m = 6
 n = 6
@@ -37,14 +36,13 @@ print('factories_output:', factories_output)
 print('wareHouse_demand:', wareHouse_demand)
 factories_location = np.random.random((m,2)) * 10
 wareHouse_location = np.random.random((n,2)) * 10
+pi = factories_location
+qj = wareHouse_location
 print('factories_location:\n{}'.format(factories_location))
 print('wareHouse_location:\n{}'.format(wareHouse_location))
 
 fac_index = ["fac_" + str(i) for i in range(m)]
 wareHouse_index = ["house_" + str(i) for i in range(n)]
-# fac_anotatation = [str(i) for i in factories_output]
-# print(fac_anotatation)
-# plot factories_output and wareHouse_demand
 #"""
 # figure of Factories Products、WareHouse Demands、Location
 # """
@@ -72,21 +70,6 @@ plt.legend(loc='upper right')
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                 wspace=0.2, hspace=0.5)
 plt.show()
-# """
-# for i in range(len(fac_anotatation)):
-#     plt.annotate(fac_anotatation[i], xy=(fac_index[i], factories_output[i]), xytext=(fac_index[i]+0.1, factories_output[i]+0.1)) # 这里xy是需要标记的坐标，xytext是对应的标签坐标
-
-#"""
-# print('factories_location:', factories_location)
-# print('wareHouse_location:', wareHouse_location)
-"""
-def cost(solution_matrix, fac_loc, ware_loc):
-    total_cost = 0
-    for i in range(m):
-        for j in range(n):
-            total_cost += solution_matrix[i][j] * np.linalg.norm(fac_loc[i] - ware_loc[j])
-    return total_cost
-"""
 x = cvx.Variable((m, n))    # 构建迭代变量
 position_distance_matrix = np.zeros((m, n))     # 存放工厂i到j的距离数据，计算两点之间的二范数
 for i in range(m):
@@ -119,22 +102,8 @@ print(result)
 fac_index = [i for i in range(m)]
 wareHouse_index = [i for i in range(n)]
 """
-color = ['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-fig = plt.figure()
-ax = Axes3D(fig)
-for i in range(m):
-    ax.scatter(fac_index, wareHouse_index, result[i], c=color[i])
-
-ax.set_zlabel('Product', fontdict={'size': 15, 'color': 'red'})
-ax.set_ylabel('House', fontdict={'size': 15, 'color': 'red'})
-ax.set_xlabel('Fac', fontdict={'size': 15, 'color': 'red'})
-
-plt.show()
+    # figure factories and wareHouse locations
 """
-
-
-
-
 fig = plt.figure(figsize=(15, 15))
 plt.scatter(factories_location[:,0], factories_location[:,1], marker='.', color='red', alpha=0.5, label='factory')
 plt.scatter(wareHouse_location[:,0], wareHouse_location[:,1], marker='*', color='blue', alpha=0.5, label='wareHouse')
@@ -142,9 +111,15 @@ plt.title('Location ')
 plt.xlabel('x')
 plt.ylabel( 'y')
 plt.legend(loc='upper right')
+"""
+    # figure factories and wareHouse locations End
+"""
+
 color = ['#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 lineShape = ['solid', 'dashed', 'dashdot', 'dotted','--', '-.']
-# """
+"""
+    # figure static transportation figure
+"""
 for i in range(m):
     x1 = factories_location[i, 0]
     y1 = factories_location[i, 1]
@@ -153,41 +128,98 @@ for i in range(m):
         if shiftNumber != 0:
             x2 = wareHouse_location[j, 0]
             y2 = wareHouse_location[j, 1]
-            # ls :'-', '--', '-.', ':', 'None', ' ', '', 'solid', 'dashed', 'dashdot', 'dotted'
             plt.plot([x1, x2], [y1, y2], c=color[j],ls=lineShape[i])
             x_annotate = (x1 + x2) / 2
             y_annotate = (y1 + y2) / 2
             plt.annotate(text='%s' % shiftNumber, xy=(x_annotate, y_annotate),
                          xytext=(x_annotate, y_annotate), textcoords='offset pixels', xycoords='data',color=color[j])
-# """
 
 """
-ln, = plt.plot([], [])
-def update(frame):
-    for i in range(m):
-        x1 = factories_location[i, 0]
-        y1 = factories_location[i, 1]
-        for j in range(n):
-            shiftNumber = result[i, j]
-            if shiftNumber != 0:
-                x2 = wareHouse_location[j, 0]
-                y2 = wareHouse_location[j, 1]
-                xData = [x1, x2]
-                yData = [y1, y2]
-                ln.set_data = (xData, yData)
-                ln.c = color[j]
-                ln.ls = lineShape[i]
-                # plt.plot([x1, x2], [y1, y2], c=color[j], ls=lineShape[i])
-                x_annotate = (x1 + x2) / 2
-                y_annotate = (y1 + y2) / 2
-                plt.annotate(text='%s' % shiftNumber, xy=(x_annotate, y_annotate),
-                             xytext=(x_annotate, y_annotate), textcoords='offset pixels', xycoords='data',
-                             color=color[j])
-                return ln,
-ani = FuncAnimation(fig, update,blit=True, interval=1)
+    # figure static transportation figure End
 """
-# plt.plot([factories_location[0,0],wareHouse_location[0,0]],[factories_location[0,1],wareHouse_location[0,1]],c='blue',ls='--')
-# plt.annotate(text='10',xy=((factories_location[0,0] + wareHouse_location[0,0])/2, (factories_location[0,1] + wareHouse_location[0,1]) / 2),
-#              xytext= ((factories_location[0,0] + wareHouse_location[0,0])/2, (factories_location[0,1] + wareHouse_location[0,1]) / 2),
-#              textcoords='offset points')
+"""
+    # figure dynamic transportation figure
+"""
+
+fig, ax = plt.subplots()
+dot =[]
+_dot =[]
+color =['r','g','b','c','violet','lightsalmon','sage','skyblue','y','pink']
+linestyle = ['o','o','o','o','o','o','o','o','o','o']
+for i in range(m):
+    for j in range(n):
+        exec("dot%s%s,=ax.plot([], [],color = color[%s],marker = linestyle[%s], animated=False)" % (i,j,i,j))
+
+def init():
+    ax.set_xlim(0,15)
+    ax.set_ylim(0,15)
+    for i in range(m):
+        for j in range (n):
+            exec("_dot.append(dot%s%s)" % (i,j))
+    return _dot
+
+def generate_dot():
+    # print("aaaa")
+    for i in range(100):
+        dot.clear()
+        for k in range(m):
+            for j in range(n):
+                if (result[k][j] > 0):
+                    x_frame = np.linspace(pi[k, 0], qj[j, 0], 100)
+                    y_frame = np.linspace(pi[k, 1], qj[j, 1], 100)
+                    newdot = [x_frame[i], y_frame[i]]
+                    # print(newdot)
+                    dot.append(newdot)
+            # print(dot)
+        yield dot
+
+def update_dot(newd):
+    _dot.clear()
+    # print(len(newd))
+    i = 0
+    for k in range(m):
+        for j in range(n):
+            if (result[k][j] > 0):
+                exec("dot%s%s.set_data(newd[i][0], newd[i][1])" % (k,j))
+                exec("_dot.append(dot%s%s)" % (k,j))
+                i += 1
+        # print(_dot)
+    return _dot
+
+def draw_line(i):
+    for j in range(n):
+        if (result[i][j] > 0):
+            x_line = np.linspace(pi[i, 0], qj[j, 0], 100)
+            y_line = np.linspace(pi[i, 1], qj[j, 1], 100)
+            ax.plot(x_line,y_line,color=color[i],linestyle='-.')
+
+def draw_location():
+    # print(pi[0, 0], qj[0, 0])
+    # print(pi[0, 1], qj[0, 1])
+
+
+    plt.scatter(pi[:, 0], pi[:, 1], marker='.', color='red',  alpha=0.5, label='factory')
+    plt.scatter(qj[:, 0], qj[:, 1], marker='*', color='blue',  alpha=0.5, label='wareHouse')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend(loc='upper right')
+
+def set_annotation(i):
+    for j in range(n):
+        if (result[i][j] > 0):
+            plt.annotate("%s" % result[i][j],
+                         xy=(((pi[i, 0]+qj[j,0])/2), ((pi[i, 1]+qj[j,1])/2)),
+                         xytext=(((pi[i, 0]+qj[j,0])/2), ((pi[i, 1]+qj[j,1])/2)),
+                         color="b",
+                       )
+draw_location()
+for i in range(m):
+    draw_line(i)
+    set_annotation(i)
+ani = FuncAnimation(fig, update_dot, frames=generate_dot, init_func=init, interval=5)
+ani.save("transportation.gif")
+"""
+    # figure dynamic transportation figure End
+"""
+
 plt.show()
